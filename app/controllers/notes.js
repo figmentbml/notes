5295ad19597b994bc563cdb7523928231386b6a7
figmentbml <beth.jaswa@gmail.com>
2015-01-26 15:48:53 -0700

    solved validations and made it look pretty

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,24 +1,23 @@
 import Ember from "ember";
-import EmberValidations from 'ember-validations';
-
-export default Ember.ObjectController.extend(EmberValidations.Mixin, {
-});
 
 export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
-      
       var body = this.get('noteCopy');
       var title = this.get('titleCopy');
 
-      var note = this.store.createRecord('note', { title: title, body: body });
+      if ((body == null) || (title == null))
+        {}
+      else
+      {var note = this.store.createRecord('note', { title: title, body: body });
       this.set('titleCopy', '');
       this.set('noteCopy', '');
-      note.save();
+      note.save();}
     },
 
     deleteNote: function() {
-      var note = this.store.find('note', this.get('id'));
+      var id = this.get('id');
+      var note = this.store.find('note', id);
       note.deleteRecord();
       note.save();
     }
