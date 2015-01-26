31cae0dcf493cd1480c3c479eb78fbb4bc2b9ecf
figmentbml <beth.jaswa@gmail.com>
2015-01-26 14:11:43 -0700

    trying to do validations

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,14 +1,25 @@
 import Ember from "ember";
+import EmberValidations from 'ember-validations';
+
+export default Ember.ObjectController.extend(EmberValidations.Mixin, {
+});
 
 export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
+      
       var body = this.get('noteCopy');
-      var title = this.get('titleCopy')
+      var title = this.get('titleCopy');
 
       var note = this.store.createRecord('note', { title: title, body: body });
       this.set('titleCopy', '');
       this.set('noteCopy', '');
+      note.save();
+    },
+
+    deleteNote: function() {
+      var note = this.store.find('note', this.get('id'));
+      note.deleteRecord();
       note.save();
     }
   }
