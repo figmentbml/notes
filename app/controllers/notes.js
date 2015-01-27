cadbe9bc7f51fb231ac4b804b6ca3adc833c32b0
figmentbml <beth.jaswa@gmail.com>
2015-01-27 13:13:00 -0700

    added flashes msgs for create, create error, and delete

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -8,12 +8,14 @@ export default Ember.ArrayController.extend({
       var title = this.get('titleCopy');
 
       if ((body == null) || (title == null)) {
-
+        this.flashMessage('fail', 'You cannot have an empty title or body.');
       } else {
         var note = this.store.createRecord('note', { title: title, body: body });
         this.set('titleCopy', '');
         this.set('noteCopy', '');
-        note.save();
+        note.save().then(function(){
+          this.flashMessage('success', 'Your note has been created!');
+        }.bind(this));
       }
     },
 
@@ -21,7 +23,7 @@ export default Ember.ArrayController.extend({
       this.store.find('note', id).then(function(note) {
         note.deleteRecord();
         note.save().then(function(){
-          this.flashMessage('success', 'Your note has been deleted!', 10000);
+          this.flashMessage('success', 'Your note has been deleted!');
         }.bind(this));
       }.bind(this));
     }
