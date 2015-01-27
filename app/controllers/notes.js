31c5adddff89dfda94f9f2772dd09d60e93af85f
figmentbml <beth.jaswa@gmail.com>
2015-01-27 09:24:31 -0700

    Renamed app in package.json to fix everythin. Installed localstorage adapter.

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -6,13 +6,14 @@ export default Ember.ArrayController.extend({
       var body = this.get('noteCopy');
       var title = this.get('titleCopy');
 
-      if ((body == null) || (title == null))
-        {}
-      else
-      {var note = this.store.createRecord('note', { title: title, body: body });
-      this.set('titleCopy', '');
-      this.set('noteCopy', '');
-      note.save();}
+      if ((body == null) || (title == null)) {
+
+      } else {
+        var note = this.store.createRecord('note', { title: title, body: body });
+        this.set('titleCopy', '');
+        this.set('noteCopy', '');
+        note.save();
+      }
     },
 
     deleteNote: function() {
