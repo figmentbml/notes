a6ef7944cc22ef337851cf33add3e6ae372b03b8
figmentbml <beth.jaswa@gmail.com>
2015-01-29 16:06:26 -0700

    updated adapter

--- config/environment.js
+++ config/environment.js
@@ -16,20 +16,10 @@ module.exports = function(environment) {
     contentSecurityPolicy: {
       'default-src': "'none'",
       'script-src': "'self' https://cdn.mxpnl.com", // Allow scripts from https://cdn.mxpnl.com
-      'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
-      'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+      'font-src': "'self' http://fonts.gstatic.com",  // Allow fonts to be loaded from http://fonts.gstatic.com
+      'connect-src': "'self' http://localhost:3000",
       'img-src': "'self'",
       'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
-      'media-src': "'self'"
-    },
-
-    contentSecurityPolicy: {
-      'default-src': "'none'",
-      'script-src': "'self'",
-      'font-src': "'self'",
-      'connect-src': "'self' localhost:3000",
-      'img-src': "'self'",
-      'style-src': "'self'",
       'media-src': "'self'"
     },
 
